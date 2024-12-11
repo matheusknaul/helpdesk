b58a7fd20150015635f316e932fda045757f6c72
matheusknaul <contatomatheush0909@gmail.com>
2024-12-11 00:24:56 -0300

    add content in classes

--- src/main/java/com/matheusknaul/helpdesk/domain/Cliente.java
+++ src/main/java/com/matheusknaul/helpdesk/domain/Cliente.java
@@ -1,4 +1,20 @@
 package com.matheusknaul.helpdesk.domain;
 
-public class Cliente {
+import com.matheusknaul.helpdesk.domain.enums.Perfil;
+
+import java.util.Set;
+
+public class Cliente extends Pessoa{
+
+    private static final long serialVersionUID = 1L;
+
+    public Cliente(){
+        super();
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Cliente(Integer id, String nome, String cpf, String email, String senha, Set<Integer> perfis){
+        super(id, nome, cpf, email, senha, perfis);
+        addPerfil(Perfil.CLIENTE);
+    }
 }
--- src/main/java/com/matheusknaul/helpdesk/domain/Tecnico.java
+++ src/main/java/com/matheusknaul/helpdesk/domain/Tecnico.java
@@ -1,4 +1,20 @@
 package com.matheusknaul.helpdesk.domain;
 
-public class Tecnico {
+import com.matheusknaul.helpdesk.domain.enums.Perfil;
+
+import java.util.Set;
+
+public class Tecnico extends Pessoa{
+
+    private static final long serialVersionUID = 1L;
+
+    public Tecnico() {
+        super();
+        addPerfil(Perfil.TECNICO);
+    }
+
+    public Tecnico(Integer id, String nome, String cpf, String email, String senha, Set<Integer> perfis){
+        super(id, nome, cpf, email, senha, perfis);
+        addPerfil(Perfil.TECNICO);
+    }
 }
